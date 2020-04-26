40cf82df4c54c0181bc4674bc29ba3d1559fa38b
Kurukuru <adidome@gmail.com>
2020-04-27 08:30:02 +0900

    abc-164-b

---
+++ abc-164-b.py
@@ -0,0 +1,13 @@
+#ABC 164B
+
+a, b, c, d = list(map(int, input().split()))
+
+while True:
+    c -= b
+    if c<=0:
+        print('Yes')
+        exit()
+    a -= d
+    if a<=0:
+        print('No')
+        exit()
