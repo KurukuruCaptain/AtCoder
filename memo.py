3adb11e86286f0e3bca7c0f064e228490f43f946
Kurukuru <adidome@gmail.com>
2020-04-28 09:44:12 +0900

    PythonMemo

---
+++ memo.py
@@ -0,0 +1,18 @@
+#####入力
+a = str(input())
+a, b, c = list(map(int, input().split()))
+
+#####リスト
+#初期化
+mylist = [0]*10
+
+#count回避
+num_list = [0]* (len(a)+1)
+for i in range(len(a)):
+#    num_list[i] = a.count(i)
+    num_list[a[i]] += 1
+
+#####MATH
+#べき乗
+math.pow(l/3, 3)
+
