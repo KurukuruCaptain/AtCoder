888ae51f0c778aa457175e05bef3751c06110dc9
Kurukuru <adidome@gmail.com>
2020-04-28 09:47:04 +0900

    AC通った！

--- abc-159-d.py
+++ abc-159-d.py
@@ -1,3 +1,23 @@
+n = int(input())
+a = list(map(int, input().split()))
+
+num_list = [0]* (len(a)+1)
+
+for i in range(len(a)):
+#    num_list[i] = a.count(i)
+    num_list[a[i]] += 1
+
+total = 0
+
+for j in range(len(num_list)):
+    total += (num_list[j]* (num_list[j]-1))/2
+
+#print(total)
+
+for k in range(len(a)):
+#    print(num_list[a[k]]) 
+    print(int(total-(num_list[a[k]])+1))
+"""
 import math
 
 n = int(input())
@@ -12,3 +32,4 @@ for i in range(1, len(a)+1):
     for j in range (1, len(a)):
         cnt += b.count(j)*(b.count(j)-1)/2
     print(int(cnt))
+"""
