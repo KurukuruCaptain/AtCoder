00640ff708b8c6de71a12ac7d403f16210a78e3f
Kurukuru <adidome@gmail.com>
2020-05-04 15:07:18 +0900

    abc-166-c

---
+++ abc-166-c.py
@@ -0,0 +1,24 @@
+n, m = list(map(int, input().split()))
+
+h = list(map(int, input().split()))
+
+yoi_warui = [0]*n
+dokuritsu = [0]*n
+
+for i in range(m):
+    a, b = list(map(int, input().split()))
+    dokuritsu[a-1] = 1
+    dokuritsu[b-1] = 1
+    if h[a-1] == h[b-1]:
+        yoi_warui[a-1] = 1
+        yoi_warui[b-1] = 1
+    elif h[a-1] < h[b-1]:
+        yoi_warui[a-1] = 1
+    elif h[b-1] < h[a-1]:
+        yoi_warui[b-1] = 1
+
+print(yoi_warui)
+print(dokuritsu)
+
+print(yoi_warui.count(0))
+
