674edd71ce4c6f8bb7e05170b3a9ca7f64d6a7a6
Kurukuru <adidome@gmail.com>
2020-05-09 15:01:25 +0900

    abc-157-c

---
+++ abc-157-c.py
@@ -0,0 +1,100 @@
+n, m = list(map(int, input().split()))
+
+s = [0]*(m+1)
+c = [0]*(m+1)
+
+for k in range(m):
+    s[k], c[k] = list(map(int, input().split()))
+
+for l in range(m):
+    if n == 1:
+        if s[l] != 1:
+            print(-1)
+            exit()
+    elif n == 2:
+        if s[l] == 3 or (s[l] == 1 and c[l] == 0):
+            print(-1)
+            exit()
+    elif n == 3:
+        if s[l] == 1 and c[l] == 0:
+            print(-1)
+            exit()
+
+if n == 1:
+    for num in range(0, 10):
+        seigo = 1
+        for i in range(m):
+            if num != c[i]:
+                seigo = 0
+        if seigo == 1:
+            print(num)
+            exit()
+    print(-1)
+    exit()
+elif n == 2:
+    for num in range(10, 100):
+        seigo = 1
+        for i in range(m):
+            if s[i] == 1:
+                if num//10 != c[i]:
+                    seigo = 0
+            if s[i] == 2:
+                if num%10 != c[i]:
+                    seigo = 0
+        if seigo == 1:
+            print(num)
+            exit()
+    print(-1)
+    exit()
+else:
+    for num in range(100, 1000):
+        seigo = 1
+        for i in range(m):
+            if s[i] == 1:
+                if num//100 != c[i]:
+                    seigo = 0
+            elif s[i] == 2:
+                if (num-100*(num//100))//10 != c[i]:
+                    seigo = 0
+            elif s[i] == 3:
+                if num%10 != c[i]:
+                    seigo = 0
+        if seigo == 1:
+            print(num)
+            exit()
+    print(-1)
+    exit()
+
+                    
+"""
+for num in range (kaishi, owari):
+    seigo = 1
+    for i in range(m):
+        if s[i] == 3:
+            if num%10 == c[i]:
+                continue
+            else:
+                seigo = 0
+        if s[i] == 2:
+            if (num//100) == 0 and (num%100)//10 == 0:
+                seigo = 0 
+            elif (num%100)//10 == c[i]:
+                continue
+            else:
+                seigo = 0
+        if s[i] == 1:
+            if c[i] == 0:
+                seigo = 0
+            elif (num//100) == c[i]:
+                continue
+            else:
+                seigo = 0
+
+    if seigo == 1:
+        print(num)
+        exit()
+        
+print(-1)
+"""
+
+
