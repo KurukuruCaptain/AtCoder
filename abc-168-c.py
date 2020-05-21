64de5ca1211608cfa538252660a249a13460af3e
Kurukuru <adidome@hotmail.com>
2020-05-22 06:37:55 +0900

    no message

--- abc-168-c.py
+++ abc-168-c.py
@@ -5,5 +5,6 @@ a, b, h, m = list(map(int, input().split()))
 a1 = math.pi*(60*h+m)/360
 b1 = math.pi*m/30
 
-print(math.sqrt( (a*math.cos(a1) - b*math.cos(b1))**2 + (a*math.sin(a1) - b*math.sin(b1))**2))
+print(math.sqrt( (a*math.cos(a1) - b*math.cos(b1))**2 + \
+    (a*math.sin(a1) - b*math.sin(b1))**2))
 
